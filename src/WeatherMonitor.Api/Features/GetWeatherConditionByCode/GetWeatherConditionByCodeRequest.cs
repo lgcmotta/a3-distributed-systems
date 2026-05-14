ce4d94198dc85ff858764643c1312d95bcc7a483
verozian <igor.loureiro8@hotmail.com>
2026-05-14 01:03:50 -0300

    changing from Query to FromRoute

--- src/WeatherMonitor.Api/Features/GetWeatherConditionByCode/GetWeatherConditionByCodeRequest.cs
+++ src/WeatherMonitor.Api/Features/GetWeatherConditionByCode/GetWeatherConditionByCodeRequest.cs
@@ -1,12 +1,10 @@
 using JetBrains.Annotations;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using WeatherMonitor.Api.Features.GetWeatherConditions;
-using WeatherMonitor.Api.Shared;
 
 namespace WeatherMonitor.Api.Features.GetWeatherConditionByCode;
 
 [UsedImplicitly]
 internal record GetWeatherConditionByCodeRequest(
-    [property: FromQuery(Name = "code")] string Code
+    [property: FromRoute(Name = "code")] string Code
 ) : IRequest<WeatherConditionByCodeResponse>;
