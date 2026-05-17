9de9b5bd70b34627431c896c6d34e40670a6b158
Luiz Motta <luiz.motta@mottacode.com>
2026-05-17 19:56:38 -0300

    fix: remove client id as required member on requests

--- src/WeatherMonitor.Api/Features/GetMonitors/GetMonitorsRequest.cs
+++ src/WeatherMonitor.Api/Features/GetMonitors/GetMonitorsRequest.cs
@@ -1,14 +1,15 @@
 using JetBrains.Annotations;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using System.Text.Json.Serialization;
 using WeatherMonitor.Api.Shared;
 
 namespace WeatherMonitor.Api.Features.GetMonitors;
 
 [UsedImplicitly]
 internal record GetMonitorsRequest(
-    [property: JsonIgnore] string ClientId,
     [property: FromQuery(Name = "page")] int Page = 1,
     [property: FromQuery(Name = "size")] int Size = 10
-) : IRequest<(IEnumerable<MonitorResponse>, PagedResponse)>;
+) : IRequest<(IEnumerable<MonitorResponse>, PagedResponse)>
+{
+    internal required string ClientId { get; init; } = string.Empty;
+}
