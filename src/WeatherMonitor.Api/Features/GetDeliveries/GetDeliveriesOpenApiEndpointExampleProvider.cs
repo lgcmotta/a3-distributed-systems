3ddeee1bd5e4999f12fb2289e33b1e7d8fd39f78
verozian <igor.loureiro8@hotmail.com>
2026-05-29 00:02:31 -0300

    fixing imports and hardcoded values

--- src/WeatherMonitor.Api/Features/GetDeliveries/GetDeliveriesOpenApiEndpointExampleProvider.cs
+++ src/WeatherMonitor.Api/Features/GetDeliveries/GetDeliveriesOpenApiEndpointExampleProvider.cs
@@ -4,6 +4,8 @@ using System.Net.Mime;
 using WeatherMonitor.Api.Contracts;
 using WeatherMonitor.Api.OpenApi.Providers;
 using WeatherMonitor.Domain.Monitors.ValueObjects;
+using WeatherMonitor.Domain.Deliveries.ValueObjects;
+using MonitorWeatherCondition = WeatherMonitor.Domain.Monitors.ValueObjects.WeatherCondition;
 
 namespace WeatherMonitor.Api.Features.GetDeliveries;
 
@@ -21,16 +23,16 @@ internal sealed class GetDeliveriesOpenApiEndpointExampleProvider : OpenApiEndpo
                     DeliveryId = Guid.Parse("8b2f1d5d-7c6f-43a2-8d7e-85f0f87e8f91"),
                     MonitorId = Guid.Parse("4e7f6d44-55e2-4cb1-9d4c-2f8b66b82e10"),
                     ForecastDate = new DateOnly(2026, 05, 24),
-                    ScheduledFor = DateTimeOffset.Parse("2026-05-24T08:30:00-03:00"),
-                    DeliveredAt = DateTimeOffset.Parse("2026-05-24T08:30:05-03:00"),
-                    Status = "Delivered",
+                    ScheduledFor = TimeZoneInfo.ConvertTime(DateTimeOffset.UtcNow, TimeZone),
+                    DeliveredAt = TimeZoneInfo.ConvertTime(DateTimeOffset.UtcNow, TimeZone).AddSeconds(5),
+                    Status = WebhookDeliveryStatus.Delivered.Value,
                     RetryCount = 0,
                     FailureReason = null,
                     CityCode = 244,
                     CityName = BrazilianState.SaoPaulo.Name,
                     State = BrazilianState.SaoPaulo.Value,
-                    WeatherConditionCode = WeatherCondition.Drizzle.Code,
-                    WeatherConditionDescription = WeatherCondition.Drizzle.Description,
+                    WeatherConditionCode = MonitorWeatherCondition.Drizzle.Code,
+                    WeatherConditionDescription = MonitorWeatherCondition.Drizzle.Description,
                     TimeZoneId = TimeZone.Id,
                     CreatedAt = TimeZoneInfo.ConvertTime(DateTimeOffset.UtcNow, TimeZone),
                     UpdatedAt = null,
